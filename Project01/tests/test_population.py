c3792b7abdd3f4e2655c83c55a8dda40c1c1ffcf
Robert Morain <robert.morain@gmail.com>
2023-01-27 11:08:25 -0700

    Added tests

---
+++ Project01/tests/test_population.py
@@ -0,0 +1,16 @@
+import unittest
+from Population import Population
+
+
+class TestPopulation(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_population(self):
+        p = Population()
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
