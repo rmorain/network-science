636786fc2bda8ab5469fb2fcfa7361ef890fa5a4
Robert Morain <robert.morain@gmail.com>
2023-01-27 13:12:07 -0700

    cleanup

--- Project01/Experiment.py
+++ Project01/Experiment.py
@@ -1,11 +1,6 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-import pandas as pd
-import seaborn as sns
-from scipy.stats import iqr
-
-sns.set()
 
 from Population import Population
 
