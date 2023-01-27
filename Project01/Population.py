9cb4ca7aba059594f53198574121c7c78b2f5fc7
Robert Morain <robert.morain@gmail.com>
2023-01-27 11:20:24 -0700

    Changed Population

--- Project01/Population.py
+++ Project01/Population.py
@@ -1,21 +1,25 @@
 import numpy as np
-import Agent
-import State
 
-class Population():
-    def __init__(self, G, m_e=1.0, s_e=1.0, m_i=2.25, s_i=0.105):
+from Agent import Agent
+from State import State
+
+
+class Population:
+    def __init__(self, G):
         N = len(G.nodes)
         self.population = []
         for i in range(N):
             neighbors = G[i]
-            self.population.append(Agent(m_e, s_e, m_i, s_i, neighbors))
+            self.population.append(Agent(i, neighbors))
         # Set 5% of the population to exposed and 5% to infectious
-        exposed_infectious = np.random.choice(self.population, size=int(N * 0.1), replace=False)
+        exposed_infectious = np.random.choice(
+            self.population, size=int(N * 0.1), replace=False
+        )
         for i in range(len(exposed_infectious) // 2):
             exposed_infectious[i].state = State.EXPOSED
         for i in range(len(exposed_infectious) // 2, len(exposed_infectious)):
             exposed_infectious[i].state = State.INFECTED
-            
+
     def count_all(self):
         susceptible = 0
         exposed = 0
@@ -31,8 +35,7 @@ class Population():
             elif a.state == State.RECOVERED:
                 recovered += 1
         return susceptible, exposed, infectious, recovered
-                
-    
+
     def step_all(self):
-        for a in self.all:
-            a.step()
+        for agent in self.population:
+            agent.step()
--- Project01/tests/test_population.py
+++ Project01/tests/test_population.py
@@ -1,15 +1,20 @@
 import unittest
+
+import networkx as nx
 from Population import Population
 
 
 class TestPopulation(unittest.TestCase):
     def setUp(self):
-        pass
+        self.G = nx.complete_graph(100)
+        self.P = Population(self.G)
 
-    def test_population(self):
-        p = Population()
+    def test_population_init(self):
+        counts = self.P.count_all()
+        self.assertEqual(counts, (90, 5, 5, 0))
 
-
+    def test_step_all(self):
+        self.P.step_all()
 
 
 if __name__ == "__main__":
