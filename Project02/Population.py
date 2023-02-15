038e39c5b51fe8c5309138b93b880b97398c0164
Robert Morain <robert.morain@gmail.com>
2023-02-15 13:09:01 -0700

    Updated population to use node ids

--- Project02/Population.py
+++ Project02/Population.py
@@ -1,54 +1,38 @@
 import networkx as nx
-import numpy as np
 
 from Agent import Agent
 from State import State
 
 
 class Population:
-    def __init__(self, G):
-        N = len(G.nodes)
-        self.population = []
+    def __init__(self, G, early_adopters):
+        self.population = {}
         for i, node in enumerate(G.nodes):
-            neighbors = [n for n in nx.all_neighbors(G, node)]
-            self.population.append(Agent(i, neighbors))
-        for i in range(N):
+            agent_id = node.id
+            neighbors = [n.id for n in nx.all_neighbors(G, node)]
+            self.population[agent_id] = Agent(agent_id, neighbors)
+        for agent in self.population.values():
             # Assign neighbors to Agent objects
-            A = self.population[i]
-            neighbors = A.neighbors
+            neighbors = agent.neighbors
             for j in range(len(neighbors)):
                 a = neighbors[j]
                 if type(a) is int:
-                    neighbors[j] = self.population[neighbors[j] - 1]
+                    neighbors[j] = self.population[neighbors[j]]
                 else:
                     # Lattice neighbors
                     neighbors[j] = self.population[10 * a[0] + a[1]]
 
-        # Set 5% of the population to exposed and 5% to infectious
-        exposed_infectious = np.random.choice(
-            self.population, size=int(N * 0.1), replace=False
-        )
-        for i in range(len(exposed_infectious) // 2):
-            exposed_infectious[i].state = State.EXPOSED
-        for i in range(len(exposed_infectious) // 2, len(exposed_infectious)):
-            exposed_infectious[i].state = State.INFECTED
         self.counts = self.count_all()
 
     def count_all(self):
-        susceptible = 0
-        exposed = 0
-        infectious = 0
-        recovered = 0
+        A = 0
+        B = 0
         for a in self.population:
-            if a.state == State.SUSCEPTIBLE:
-                susceptible += 1
-            elif a.state == State.EXPOSED:
-                exposed += 1
-            elif a.state == State.INFECTED:
-                infectious += 1
-            elif a.state == State.RECOVERED:
-                recovered += 1
-        return susceptible, exposed, infectious, recovered
+            if a.state == State.A:
+                A += 1
+            elif a.state == State.B:
+                B += 1
+        return A, B
 
     def step_all(self):
         for agent in self.population:
