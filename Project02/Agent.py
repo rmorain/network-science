850c6b4f865fe7f14a306d7a34aa395325edaecc
Vigynesh Bhatt <vigyneshbhatt@gmail.com>
2023-02-17 13:05:47 -0700

    added prev_state & current_state to Agent

--- Project02/Agent.py
+++ Project02/Agent.py
@@ -13,19 +13,21 @@ class Agent:
     def __init__(self, id, neighbors, initial_state=State.STATE_B):
         self.id = id
         self.neighbors = neighbors
-        self.state = initial_state
+        self.prev_state = initial_state
+        self.current_state = initial_state
 
     def setState(self, state):
-        self.state = state
+        self.prev_state = self.current_state
+        self.current_state = state
 
     def getState(self):
-        return self.state
+        return self.current_state
 
     def step(self):
-        if self.state == State.STATE_B:
+        if self.current_state == State.STATE_B:
             ua,ub=0,0
             for neighbor in self.neighbors:
-                if neighbor.state == State.STATE_B:
+                if neighbor.current_state == State.STATE_B:
                     ub+=self.payoff_b
                 else:
                     ua+=self.payoff_a
